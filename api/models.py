98c36240d40c9f574beecb8d2e4619adcca63c27
Alex <56600569+alex-pa@users.noreply.github.com>
2020-09-22 16:43:38 +0300

    first commit

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from tastypie.resources import ModelResource
+from movies.models import Movies
+
+
+class MovieResource(ModelResource):
+    class Meta:
+        queryset = Movies.objects.all()
+        resource_name = 'movies'
+        excludes = ['date_created']
---
+++ movies/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Movies
+
+
+def index(request):
+    movies = Movies.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movies, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
